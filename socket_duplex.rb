6d95f06cef66bf65d7d5a7c40bd387af475f0833
Michael Vilensky <michael@secful.com>
2016-01-19 18:54:04 -0800

    Building json representing the data

--- socket_duplex.rb
+++ socket_duplex.rb
@@ -1,4 +1,3 @@
-require "socket"
 require 'json'
 require 'websocket-client-simple'
 
@@ -8,16 +7,19 @@ module Rack
 
     def initialize(app, socket_path)
       @app, @socket_path = app, socket_path
+      Thread.new { connect_to_ws() }
     end
 
     def call(env)
-      Thread.new { connect_to_ws() }
-      dup._call(env)
+      begin
+        Thread.new { connect_to_ws() }
+        dup._call(env)
+      rescue
+      rescue Exception
+      end
     end
 
-
     def _call(env)
-      Thread.new{foo()}
       status, headers, body = @app.call(env)
       Thread.new{ handle_request(env) }
       Thread.new{ handle_response(status, headers, body)}
@@ -28,61 +30,48 @@ module Rack
 
     def connect_to_ws
       if !@ws or !@ws.open?
-        puts "Connecting..."
         @ws = WebSocket::Client::Simple.connect 'ws://127.0.0.1:8000'
       end
     end
 
     def handle_request(env)
       request_hash = {}
-      print "ZZZ1"
-      @ws.send "Ruby!!!"
-      puts "Sending..."
-      print "ZZZ2"
-    end
-
-    def _handle_request(env)
       if env["rack.url_scheme"] == "http"
-        UNIXSocket.open(@socket_path) do |socket|
-          write_env socket, env
-        end
+        write_env(request_hash, env)
+        @ws.send request_hash.to_json
       end
     end
 
     def handle_response(status, headers, body)
-
     end
 
-    def write_env(socket, env)
-      write_request_line socket, env
-      write_headers      socket, env
-      socket << "\r\n" # empty line
-      @ws.send "\r\n" # empty line
-      write_post_body socket, env
+    def write_env(request_hash, env)
+      write_request_line request_hash, env
+      write_headers      request_hash, env
+      write_post_body    request_hash, env
     end
 
-    def write_request_line(socket, env)
+    def write_request_line(request_hash, env)
       path_with_query_string = env["PATH_INFO"]
       path_with_query_string << "?#{env["QUERY_STRING"]}" if env["QUERY_STRING"].length > 0
-
-      socket << "#{env["REQUEST_METHOD"]} #{path_with_query_string} #{env["HTTP_VERSION"] || "HTTP/1.0"}\r\n"
-      @ws.send "#{env["REQUEST_METHOD"]} #{path_with_query_string} #{env["HTTP_VERSION"] || "HTTP/1.0"}\r\n"
+      request_hash[:http] = {path: path_with_query_string,
+                             version: env["HTTP_VERSION"],
+                             method: env["REQUEST_METHOD"]}
     end
 
-    def write_headers(socket, env)
+    def write_headers(request_hash, env)
       headers = env.select { |k, v| k =~ /^HTTP_|CONTENT_/ }
       headers.delete("HTTP_VERSION")
-      
+
+      request_hash[:http][:headers] = headers_arr = []
       headers.each do |k,v|
-        socket << "#{k.gsub(/^HTTP_/, "").gsub("_", "-")}: #{v}\r\n"
-        @ws.send "#{k.gsub(/^HTTP_/, "").gsub("_", "-")}: #{v}\r\n"
+        headers_arr << {key: k, value: v}
       end
     end
 
-    def write_post_body(socket, env)
+    def write_post_body(request_hash, env)
       if env["CONTENT_LENGTH"] && (content_length = env["CONTENT_LENGTH"].to_i) > 0
-        socket << env["rack.input"].read
-        @ws.send env["rack.input"].read
+        request_hash[:htp][:payload] = env["rack.input"].read
         env["rack.input"].rewind
       end
     end
